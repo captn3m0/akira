a5b94ecff4c672f2937fd73e01351dd6b986cc76
Abhishek <das.abhshk@gmail.com>
2012-03-24 09:54:45 +0530

    added hall of fame & login on homepage

--- assets/www/assets/js/main.js
+++ assets/www/assets/js/main.js
@@ -9,9 +9,26 @@ $('#peopleli').click( function() {
 		html='';
 		for (i in data)
 		{
-				html+='<li><div class="name">'+data[i].name+'</div><div class="number">'+data[i].number+'</div><div class="car">'+data[i].car+'</div><div class="distance">'+data[i].distance+' miles</div><div class="image"><img src='+data[i].url+'></div></li>';
+			html+='<li><div class="name">'+data[i].name+'</div><div class="number">'+data[i].number+'</div><div class="car">'+data[i].car+'</div><div class="distance">'+data[i].distance+' miles</div><div class="image"><img src='+data[i].url+'></div></li>';
 		}
 		$('#gallery').html(html);
 	});
 });
+
+$('#hofli').click( function() {
+	
+	$.getJSON('path', function(data) {
+		
+		console.log('done');
+		html='';
+		rank=1;
+		for (i in data)
+		{
+			html+='<li><div class="rank">'+rank+'</div><div class="name">'+data[i].name+'</div><div class="number">'+data[i].number+'</div><div class="car">'+data[i].car+'</div><div class="distance">'+data[i].distance+' miles</div><div class="image"><img src='+data[i].url+'></div></li>';
+		}
+		$('#hof').html(html);		
+	});
+});
+
+
 
