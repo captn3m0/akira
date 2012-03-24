11e99524be35227fa7bc005a2605865c9db7ddf6
Aditya Garg <adityagargg@gmail.com>
2012-03-24 12:38:48 +0530

    added mapping functions

--- assets/www/assets/js/geo.js
+++ assets/www/assets/js/geo.js
@@ -6,6 +6,7 @@
 	y="";
 
 	function startLocating(){
+		initialize();
 		getLocation();
 		interval=setInterval(getLocation, 300000)}
 
@@ -64,10 +65,11 @@
         states[Connection.NONE]     = 'No network connection';*/
 		if(networkState=="Connection.NONE"){
 			var coord = LawnChair(function(){
-				this.batch(coordinate,function(){console.log('coordinates stored');} //work on it
+				this.batch(coordinate,function(){console.log('coordinates stored');} //work on it ====>important
 				)});
 			}
-		else ajaxPost();}
+		else {ajaxPost();
+		calculateDistances()}}
 	}
 	
 	
@@ -86,13 +88,101 @@
 	});
 	}
 	
-	function distTravel(){};
+	
 
 	function stopLocating(){clearInterval(interval);}
 	
 	
-	
+      var map;
+      var geocoder;
+      var bounds = new google.maps.LatLngBounds();
+      var markersArray = [];
+      
+      var origin = new google.maps.LatLng(55.930385, -3.118425);
+      var destination = new google.maps.LatLng(50.087692, 14.421150);
 
+      var destinationIcon = "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=D|FF0000|000000";
+      var originIcon = "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=O|FFFF00|000000";
+
+      function initialize() {
+        var opts = {
+          center: new google.maps.LatLng(29.964354, 78.173543),
+          zoom: 10,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById('map'), opts);
+        geocoder = new google.maps.Geocoder();
+      }
+    
+      function calculateDistances() {
+        var service = new google.maps.DistanceMatrixService();
+        service.getDistanceMatrix(
+          {
+            origins: [origin],
+            destinations: [destination],
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: google.maps.UnitSystem.METRIC,
+            avoidHighways: false,
+            avoidTolls: false
+          }, callback);
+      }
+
+      function callback(response, status) {
+        if (status != google.maps.DistanceMatrixStatus.OK) {
+          alert('Error was: ' + status);
+        } else {
+          var origins = response.originAddresses;
+          var destinations = response.destinationAddresses;
+          var outputDiv = document.getElementById('outputDiv');
+          outputDiv.innerHTML = '';
+          deleteOverlays();
+
+          for (var i = 0; i < origins.length; i++) {
+            var results = response.rows[i].elements;
+            addMarker(origins[i], false);
+            for (var j = 0; j < results.length; j++) {
+              addMarker(destinations[j], true);
+              outputDiv.innerHTML += origins[i] + " to " + destinations[j]
+                  + ": " + results[j].distance.text + " in "
+                  + results[j].duration.text + "<br />";
+            }
+          }
+        }
+      }
+
+      function addMarker(location, isDestination) {
+        var icon;
+        if (isDestination) {
+          icon = destinationIcon;
+        } else {
+          icon = originIcon;
+        }
+        geocoder.geocode({'address': location}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            bounds.extend(results[0].geometry.location);
+            map.fitBounds(bounds);
+            var marker = new google.maps.Marker({
+              map: map,
+              position: results[0].geometry.location,
+              icon: icon
+            });
+            markersArray.push(marker);
+          } else {
+            alert("Geocode was not successful for the following reason: "
+              + status);
+          }
+        });
+      }
+      
+      function deleteOverlays() {
+        if (markersArray) {
+          for (i in markersArray) {
+            markersArray[i].setMap(null);
+          }
+          markersArray.length = 0;
+        }
+      }
+	
 
 
 
